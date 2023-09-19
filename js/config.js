d3b580e9438a9a96dc1c1c115d0bce40fe6227c4
seanZealogics <Sean.Chang@zealogics.co>
2023-09-19 15:49:07 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
   cognito: {
-    userPoolId: "us-east-1_MX1zjZbgi", // e.g. us-east-2_uXboG5pAb
-    userPoolClientId: "3jq7qohgj4kf8359ggj1pcvl46", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-    region: "us-east-1", // e.g. us-east-2
+    userPoolId: "ap-northeast-1_ydq8sagKB", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "3eee4n4c6om19ckd3uaqp7a3i4", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "ap-northeast-1", // e.g. us-east-2
   },
   api: {
     invokeUrl: "https://grspkumd6j.execute-api.us-east-1.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
