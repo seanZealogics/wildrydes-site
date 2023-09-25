0b34bb72d014808c48b5d502d50ccd3c36071ea9
seanZealogics <Sean.Chang@zealogics.co>
2023-09-25 14:21:05 +0800

    add main page

--- js/cognito-auth.js
+++ js/cognito-auth.js
@@ -121,7 +121,7 @@ var WildRydes = window.WildRydes || {};
         signin(email, password,
             function signinSuccess() {
                 console.log('Successfully Logged In');
-                window.location.href = 'ride.html';
+                window.location.href = 'main.html';
             },
             function signinError(err) {
                 alert(err);
