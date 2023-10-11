faed65652f41c676c3cf365d3072899159be7634
seanZealogics <Sean.Chang@zealogics.co>
2023-10-11 16:50:42 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
     region: "ap-southeast-2", // e.g. us-east-2
   },
   api: {
-    invokeUrl: "https://wrg404fuwj.execute-api.ap-southeast-2.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-  },
+    invokeUrl: "https://q8jwptu61k.execute-api.ap-southeast-2.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  }
 };
