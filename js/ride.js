0af7877c3e1d40c6f60a2f983442c89cf88b48d3
seanZealogics <Sean.Chang@zealogics.co>
2023-10-11 12:40:22 +0800

    Update ride.js

--- js/ride.js
+++ js/ride.js
@@ -18,7 +18,7 @@ WildRydes.map = WildRydes.map || {};
     function requestUnicorn(pickupLocation) {
         $.ajax({
             method: 'POST',
-            url: _config.api.invokeUrl + '/test`',
+            url: _config.api.invokeUrl + '/ride',
             headers: {
                 Authorization: authToken
             },
