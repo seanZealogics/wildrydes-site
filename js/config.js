622df4b35e4ca81e1e0bf4ffba4ff6d74fe5d97d
seanZealogics <Sean.Chang@zealogics.co>
2023-10-11 16:06:06 +0800

    restore to origin

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
   cognito: {
-    userPoolId: "ap-northeast-1_ydq8sagKB", // e.g. us-east-2_uXboG5pAb
-    userPoolClientId: "3eee4n4c6om19ckd3uaqp7a3i4", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-    region: "ap-northeast-1", // e.g. us-east-2
+    userPoolId: "ap-southeast-2_fsU6U095m", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "4qaiscvcmijp4dlfj9b1g5080l", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "ap-southeast-2", // e.g. us-east-2
   },
   api: {
     invokeUrl: "https://wrg404fuwj.execute-api.ap-southeast-2.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
