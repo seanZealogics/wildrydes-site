7b93911d3ebf58fa39f0bbbad0b2d285af890998
seanZealogics <Sean.Chang@zealogics.co>
2023-10-12 23:57:55 +0800

    Update ride.js

--- js/ride.js
+++ js/ride.js
@@ -8,11 +8,8 @@
 		  "url": "https://6sjd6e7vlefx6o5bqdqv2jsj2u0zlygb.lambda-url.ap-northeast-1.on.aws?bucket_name=zealogics-resume&object_key=Resumes/Alvin Pon.pdf",
 		  "method": "GET",
 		  "timeout": 0,
-		  "headers": {
-			"bucket_name": "zealogics-resume",
-			"object_key": "Resumes/Alvin Pon.pdf"
-		  },
 		};
+		
 		$.ajax(settings).done(function (response) {
 		  console.log(response);
 		  displayUpdate(response);
